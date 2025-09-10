12b6efc36902e57b5bdc2ea39d668552e9e0cbc4
cmartyr <cjoycemartyr@gmail.com>
2025-09-10 16:35:55 +0930

    feat: added database message send

--- secure-chat/server/database.js
+++ secure-chat/server/database.js
@@ -5,28 +5,25 @@ var client;
 
 class DatabaseManager {
 	constructor() {
-
+		client = new MongoClient(uri);
 	}
+
+	//probs should sanitise details somewhere here at some point
+	//figure out how file sending would work?
 
 	// returns null if user does not exist
 	// use then to resolve promise:
 	// eg: dbManager.getUsername("test2").then(function(value) {console.log(value)});
 	async getUser(username) {
-	try {
-		client = new MongoClient(uri);
 		const database = client.db('28test');
 		const users = database.collection('users');
 		const query = await users.findOne({ 'username': username });
 		return(query);
-	} finally {
-		await client.close();
-	}
 	}
 
 	//userDetails should be an object already
+	//fields: username, password, email, active
 	async addUser(userDetails) {
-	try {
-		client = new MongoClient(uri);
 		const database = client.db('28test');
 		const collection = database.collection('users');
 		if (await collection.findOne({'username': userDetails.username}) != null)
@@ -36,9 +33,35 @@ class DatabaseManager {
     		database.close();
   		});
 	}
-	finally {
-		await client.close();
+
+	//fields: from, to, content
+	//add file functionality later
+	async addMessage(messageDetails) {
+		const database = client.db('28test');
+		const collection = database.collection('messages');
+		const query = {
+			'time': new Date(),
+			'content': messageDetails.content
+		}
+		if (await collection.findOne({'username': messageDetails.from}) == null)
+			return (null); //user dne
+		if (await collection.findOne({'username': messageDetails.to}) != null)
+			return (null); //user dne
+		this.getUser(messageDetails.from).then(function(value) {
+			query['from'] = value["_id"];
+			console.log()
+		});
+		this.getUser(messageDetails.to).then(function(value) {
+			query['to'] = value["_id"];
+		});
+		collection.insertOne(query, function(err, res) {
+    		if (err) throw err;
+    		database.close();
+  		});
 	}
+
+	async endClient() {
+		await client.close();
 	}
 }
 
