8d8c180f8fd0f7e2b34cc6751d018dbd79ed67ae
Grace Baek <a1904965@adelaide.edu.au>
2025-10-05 20:50:00 +1030

    deleted the secure-chat directory

--- secure-chat/server/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET LOGIN page. */
-router.get('/', function(req, res) {
-  res.redirect('/login.html');
-});
-
-module.exports = router;
