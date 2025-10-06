3ebfe0a353273c419fd5a325c66179284ec1eddd
cmartyr <cjoycemartyr@gmail.com>
2025-10-06 21:36:19 +1030

    able to send and receive server announcements between servers

--- Chat28/introducer.js
+++ Chat28/introducer.js
@@ -1,10 +1,25 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
 
-let connectedIntroducer = null;
+const { address } = require("./ws");
+
+let connectedIntroducer = false;
 let bootstrapServers;
 
 const server_id = "chat_28";
+const serverList = [];
+
+function connectServers() {
+	parseBootstrapServers();
+	connectToIntroducer();
+	if (connectedIntroducer)
+	{
+		openCommunications();
+		sendServerAnnouncement();
+		return (true);
+	}
+	return (false);
+}
 
 function parseBootstrapServers() {
 	try {
@@ -46,8 +61,14 @@ function connectToIntroducer() {
 				if (data.type == "SERVER_WELCOME")
 				{
 					console.log("Connected successfully to Introducer");
-					connectedIntroducer = true;
-					server_id = data.payload.assigned_id;
+					try {
+						connectedIntroducer = true;
+						server_id = data.payload.assigned_id;
+						serverList = data.payload.clients;
+					}
+					catch {
+						connectedIntroducer = false;
+					}
 				}
 			});
 		}
@@ -64,4 +85,68 @@ function connectToIntroducer() {
 		console.log("Failed to connect to Introducer; Running on sole local server instance.");
 }
 
-module.exports = { parseBootstrapServers, connectToIntroducer };
+function openCommunications() {
+	const socket = new WebSocket("ws://localhost:433");
+
+	socket.addEventListener("open", (event) => {
+		console.log(socket.remo)
+	});
+
+	socket.addEventListener("message", (event) => {
+		const data = JSON.parse(event.data);
+		if (data.type)
+			parseMessages(data);
+	});
+}
+
+function sendServerAnnouncement() {
+	for (i = 0; i < serverList.length; i++)
+	{
+		try {
+			var ipString = serverList[i].host + ":" + serverList[i].port;
+			var server = new WebSocket("ws://" + ipString);
+
+			server.onopen = function(open) {
+  				server.send(JSON.stringify({
+					"type":"SERVER_ANNOUNCE",
+					"from":server_id,
+					"to":serverList[i].user_id,
+					"ts":1700000000000,
+					"payload":{
+						"host":address,
+						"port":433,
+						"pubkey":bootstrapServers[i].pubkey
+					},
+					"sig":"..."
+				}));
+			}
+		}
+		catch {
+			console.log("Failed to connect to external Server.");
+			continue;
+		}
+	}
+}
+
+function parseMessages(message) {
+	switch (message.type)
+	{
+		case "SERVER_ANNOUNCE":
+			try {
+				serverList.push({
+					"user_id": message.from,
+					"host": message.payload.host,
+					"port": message.payload.port,
+					"pubkey": message.payload.pubkey
+				});
+			}
+			catch {
+				break;
+			}
+			break;
+		default:
+			break;
+	}
+}
+
+module.exports = { connectServers };
